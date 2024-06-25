2edc56f4ca5b370c63153157062b9ec813747353
AliMantach <alimantach11@gmail.com>
2024-06-25 05:28:09 +0300

    testprimeeng

--- GymManagement/src/app/home/home.component.ts
+++ GymManagement/src/app/home/home.component.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { MenuModule } from 'primeng/menu';
+import { MenubarModule } from 'primeng/menubar';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { RippleModule } from 'primeng/ripple';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,MenuModule,MenubarModule,BadgeModule, AvatarModule ,RippleModule,FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  constructor(private formBuilder: FormBuilder){};
+  items = [
+    {
+      label: 'File',
+    
+      items: [
+        { label: 'New' },
+        { label: 'Open' },
+       
+      ]
+    },
+    {
+      label: 'Edit',
+    
+      items: [
+        { label: 'Undo',  },
+        { label: 'Redo' }
+      ]
+    },
+    {
+      label: 'Help',
+     
+      items: [
+        { label: 'Contents' },
+        { label: 'Search' },
+        { label: 'New' },
+        { label: 'Open' }
+      ]
+    }
+  ];
 
 }
